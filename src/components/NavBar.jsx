50398eb7cccc0710d4246b864d22384a3dcf66fb
wooniekim <dawun3295@gmail.com>
2023-05-29 10:05:25 +0900

    Add hover animation bottom navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,42 +6,40 @@ const NavBar = () => {
     <div className="fixed bottom-0 w-screen">
       <footer className="p-2 bg-gray-900 rounded-lg shadow">
         <ul className="flex flex-wrap items-center justify-center mt-2">
-          <li className="px-4 grid place-items-center">
-            <FaHome className="text-orange-500 mb-1" />
-            <NavLink
-              to={"/"}
-              className="text-sm text-orange-500 hover:underline decoration-red-300"
-            >
-              Home
-            </NavLink>
-          </li>
-          <li className="px-4 grid place-items-center">
-            <FaClipboardList className="text-orange-500 mb-1" />
-            <NavLink
-              to={"/board_list"}
-              className="text-sm text-orange-500 hover:underline decoration-red-300"
-            >
+          <NavLink
+            to={"/"}
+            className="transition px-4 grid place-items-center hover:scale-110 hover:-translate-y-1"
+          >
+            <FaHome className="text-orange-500 mb-1 text-xl" />
+            <h1 className="text-sm text-orange-500 decoration-red-300">Home</h1>
+          </NavLink>
+          <NavLink
+            to={"/board_list"}
+            className="transition px-4 grid place-items-center hover:scale-110 hover:-translate-y-1"
+          >
+            <FaClipboardList className="text-orange-500 mb-1 text-xl" />
+            <span className="text-sm text-orange-500 decoration-red-300">
               List
-            </NavLink>
-          </li>
-          <li className="px-4 grid place-items-center">
-            <FaPen className="text-orange-500 mb-1" />
-            <NavLink
-              to={"/board_write"}
-              className="text-sm text-orange-500 hover:underline decoration-red-300"
-            >
+            </span>
+          </NavLink>
+          <NavLink
+            to={"/board_write"}
+            className="transition px-4 grid place-items-center hover:scale-110 hover:-translate-y-1"
+          >
+            <FaPen className="text-orange-500 mb-1 text-xl" />
+            <span className="text-sm text-orange-500 decoration-red-300">
               Write
-            </NavLink>
-          </li>
-          <li className="px-4 grid place-items-center">
-            <FaUser className="text-orange-500 mb-1" />
-            <NavLink
-              to={"/board_update"}
-              className="text-sm text-orange-500 hover:underline decoration-red-300"
-            >
-              Update
-            </NavLink>
-          </li>
+            </span>
+          </NavLink>
+          <NavLink
+            to={"/board_update"}
+            className="transition px-4 grid place-items-center hover:scale-110 hover:-translate-y-1"
+          >
+            <FaUser className="text-orange-500 mb-1 text-xl" />
+            <span className="text-sm text-orange-500 decoration-red-300">
+              MyPage
+            </span>
+          </NavLink>
         </ul>
       </footer>
     </div>
